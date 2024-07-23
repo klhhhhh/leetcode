45a748dff61521f1f4adbcdcd9c738d3c3685f2e
klhhhhh <1412841649@qq.com>
2024-07-23 14:07:33 +0800

    300.longest-increasing-subsequence

--- 300.longest-increasing-subsequence/300.longest-increasing-subsequence.cpp
+++ 300.longest-increasing-subsequence/300.longest-increasing-subsequence.cpp
@@ -8,11 +8,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int max_len = 1;
-        int end = nums[0];
-        for(int i = 1; i < nums.size(); ++i) {
-            
+        int maxlen = 1;
+        vector<int> dp(nums.size(), 1);
+        for(int i = 0; i < nums.size(); ++i) {
+            for(int j = 0; j < i; ++j) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+            maxlen = max(dp[i], maxlen);
         }
+        return maxlen;
+
     }
 };
 // @lc code=end
