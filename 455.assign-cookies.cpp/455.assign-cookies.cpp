b936a587560238e7b59cf6f9863bfd337540aeb3
klhhhhh <1412841649@qq.com>
2024-07-23 17:15:22 +0800

    455.assign-cookies.cpp

---
+++ 455.assign-cookies.cpp/455.assign-cookies.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=455 lang=cpp
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+    }
+};
+// @lc code=end
+
