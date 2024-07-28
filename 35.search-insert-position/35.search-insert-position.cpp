d689b00ec28040165aa7ba5debf7cf1b9fabc896
klhhhhh <1412841649@qq.com>
2024-07-28 16:21:31 +0800

    35.search-insert-position

---
+++ 35.search-insert-position/35.search-insert-position.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int right = nums.size() - 1;
+        int left  = 0;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(target < nums[mid]){
+                right = mid - 1;
+            }
+            else if(target > nums[mid]){
+                left = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return left;
+    }
+};
+// @lc code=end
+
