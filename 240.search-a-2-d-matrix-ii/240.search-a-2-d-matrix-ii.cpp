e410e763907487bd30a91f142693c77e897fedd2
klhhhhh <1412841649@qq.com>
2024-07-28 15:51:29 +0800

    240.search-a-2-d-matrix-ii

---
+++ 240.search-a-2-d-matrix-ii/240.search-a-2-d-matrix-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=240 lang=cpp
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+    }
+};
+// @lc code=end
+
