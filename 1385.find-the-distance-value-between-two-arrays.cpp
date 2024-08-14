353fa22bd5c4a330674e587008d7a6ec1009f106
klhhhhh <1412841649@qq.com>
2024-08-14 11:39:56 +0800

    add some nots link

---
+++ 1385.find-the-distance-value-between-two-arrays.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1385 lang=cpp
+ *
+ * [1385] Find the Distance Value Between Two Arrays
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        
+    }
+};
+// @lc code=end
+
